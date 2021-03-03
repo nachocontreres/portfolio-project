8aa9572269da32f4a6d31d2423616c83d01a24bd
Ignacio Contreres Kirkby <ignaciocontrereskirkby@MacBook-Pro-de-Ignacio.local>
2021-03-03 13:29:59 -0300

    header and particles ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Particles from 'react-particles-js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
+      <Particles
+        params={{
+          particles:{
+            number:{
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            }
+           
+          }
+      }} 
+      />
      <Navbar></Navbar>
+     <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <Typed
+          className="typed-text"
+          strings={["Hi, ", "I'm Nacho, Frontend Developer"]}
+          typeSpeed={50}
+          backSpeed={50}
+        />
+
+        <a href="#" className="btn-contact push">Contact me</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
